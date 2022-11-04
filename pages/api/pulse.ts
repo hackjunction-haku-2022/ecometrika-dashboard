fc6b2f955aa3ecd2de95a2207e1d1781f209be12
Алексей А. Хлебаев <akhlebaev@yoomoney.ru>
2022-11-04 21:14:53 +0200

    creating-endpoints

--- pages/api/pulse.ts
+++ pages/api/pulse.ts
@@ -2,12 +2,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  status: string
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log('pulse: ', req.method, 'body: ', req.body);
+
+  res.status(200).json({status: 'success'});
 }
