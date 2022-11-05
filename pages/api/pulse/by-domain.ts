73d0b275607a2a07c77d287866a2bfe5c1f161ff
Алексей А. Хлебаев <akhlebaev@yoomoney.ru>
2022-11-05 11:17:05 +0200

    ts-fixes

--- pages/api/pulse/by-domain.ts
+++ pages/api/pulse/by-domain.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { getPulseByDomain } from '@/lib/db';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { getPulseByDomain } from '../../../lib/db';
 
 export default async function handler(
   req: NextApiRequest,
--- pages/api/site.ts
+++ pages/api/site.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { updateSite } from '@/lib/db';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { updateSite } from '../../lib/db';
+
 
 type Data = {
   status: string
